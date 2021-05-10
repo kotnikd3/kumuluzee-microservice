36cfe73442b07258c58a62e700880cd0bc7733cf
Denis Kotnik <kotnikd@cgs-labs.com>
2021-05-10 17:08:25 +0200

    Dodan open-api3

--- api/src/main/java/si/kotnik/api/v1/NakupovalniSeznamiApplication.java
+++ api/src/main/java/si/kotnik/api/v1/NakupovalniSeznamiApplication.java
@@ -1,8 +1,18 @@
 package si.kotnik.api.v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@OpenAPIDefinition(info = @Info(title = "Nakupovalni seznami API",
+        version = "v1",
+        contact = @Contact(email = "denis.kotnik@gmail.com"), license = @License(name = "dev"), description = "API za nakupovalni seznam"),
+        servers = @Server(url = "http://localhost:8080/"))
 @ApplicationPath("v1")
 public class NakupovalniSeznamiApplication extends Application {
 }
